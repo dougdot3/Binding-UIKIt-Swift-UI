0f9713b790d6b96a82b1db871f916c619cc0c3d8
dougdougdot3 <doug@dot-3.net>
2019-11-27 08:35:16 -0600

    Initial Commit

---
+++ Binding UIKIt Swift UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Binding UIKIt Swift UI
+//
+//  Created by Doug on 2019-11-27.
+//  Copyright © 2019 dot3 Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
